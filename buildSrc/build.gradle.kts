474bccf5e7403fd9adc3945991bcd5e8cb11c3ef
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-01-30 01:58:54 +0700

    fix

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.3.1")
+    implementation("com.android.tools.build:gradle:7.4.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
 }
