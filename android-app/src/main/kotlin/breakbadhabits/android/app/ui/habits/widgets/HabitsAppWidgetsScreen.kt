aae5734c2186c0c0ce50d3f3a506590d5a105693
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2023-03-31 12:59:05 +0700

    little save

--- android-app/src/main/kotlin/breakbadhabits/android/app/ui/habits/widgets/HabitsAppWidgetsScreen.kt
+++ android-app/src/main/kotlin/breakbadhabits/android/app/ui/habits/widgets/HabitsAppWidgetsScreen.kt
@@ -2,6 +2,7 @@ package breakbadhabits.android.app.ui.habits.widgets
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import breakbadhabits.android.app.R
 import breakbadhabits.app.logic.habits.entity.HabitAppWidgetConfig
@@ -39,20 +42,33 @@ fun HabitAppWidgetsScreen(
                 priority = Text.Priority.High
             )
 
-            LazyColumn(
-                contentPadding = PaddingValues(
-                    start = 16.dp,
-                    end = 16.dp,
-                    top = 16.dp,
-                    bottom = 160.dp
-                ),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                items(widgets, key = { it.config.id.value }) {
-                    WidgetConfigItem(
-                        item = it,
-                        onClick = { onWidgetClick(it.config.id) }
+            if (widgets.isEmpty()) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        modifier = Modifier.padding(16.dp),
+                        textAlign = TextAlign.Center,
+                        text = stringResource(R.string.habitsAppWidgets_empty)
                     )
+                }
+            } else {
+                LazyColumn(
+                    contentPadding = PaddingValues(
+                        start = 16.dp,
+                        end = 16.dp,
+                        top = 16.dp,
+                        bottom = 160.dp
+                    ),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(widgets, key = { it.config.id.value }) {
+                        WidgetConfigItem(
+                            item = it,
+                            onClick = { onWidgetClick(it.config.id) }
+                        )
+                    }
                 }
             }
         }
