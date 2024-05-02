80a2e9f464165fa94cddae8350bfa02834a0155d
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2024-05-02 11:19:39 +0300

    little save

--- src/iosMain/kotlin/epicarchitect/breakbadhabits/IosApp.kt
+++ src/iosMain/kotlin/epicarchitect/breakbadhabits/IosApp.kt
@@ -1,6 +1,7 @@
 package epicarchitect.breakbadhabits
 
 import androidx.compose.ui.window.ComposeUIViewController
+import epicarchitect.breakbadhabits.ui.Root
 
 @Suppress("FunctionName", "unused")
 fun AppViewController() = ComposeUIViewController { Root() }
