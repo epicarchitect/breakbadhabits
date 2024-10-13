ad3dacff5218db9d56bb6d46ec8bec5d307fa671
Alexander Kolmachikhin <alexanderkolmachikhin@gmail.com>
2024-10-13 19:48:12 +0700

    fix widgets

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,7 +78,7 @@ android {
             isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "src/android-proguard-rules.pro"
+//                "src/android-proguard-rules.pro"
             )
         }
 
